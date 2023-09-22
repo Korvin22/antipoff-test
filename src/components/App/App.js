77c9f237cbbd3b6cbc92f23703e874cac2a51392
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-09-22 07:31:46 +0300

    feature: main component added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import Header from "../Header/Header";
-import Footer from "../Footer/Footer"
+import Footer from "../Footer/Footer";
 import { Routes, useLocation } from "react-router-dom";
 import Register from "../Register/Register";
+import Main from "../Main/Main";
 import { Route } from "react-router-dom";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -11,10 +12,37 @@ import { apiAuth } from "../../utils/Api.auth";
 function App(props) {
   const [message, setMessage] = useState("");
   const [successReg, setSuccessReg] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
   const navigate = useNavigate();
 
-  
-  
+  function handleLogin() {
+    setLoggedIn(true);
+    localStorage.setItem("loggedIn", loggedIn);
+    console.log(localStorage.loggedIn);
+  }
+
+  function handleUpdateAutharization(data) {
+    apiAuth
+      .authorize(data.email, data.password)
+      .then((res) => {
+        localStorage.setItem("token", res.token);
+        localStorage.setItem("email", res.email);
+        const jwt = localStorage.getItem("token");
+        if (jwt) {
+          apiAuth
+            .checkToken(jwt)
+            .then((res) => {
+              setLoggedIn(true);
+              navigate("/");
+            })
+            .catch((err) => console.log(err));
+        }
+        handleLogin();
+        navigate("/");
+      })
+      .catch((err) => setMessage(err));
+  }
+
   function handleUpdateRegistration(data) {
     apiAuth
       .register(data.name, data.email, data.password)
@@ -30,23 +58,25 @@ function App(props) {
         setSuccessReg(false);
         navigate("/signup");
       })
-      .finally(() => {
-      });
+      .finally(() => {});
   }
 
   return (
     <div className="app">
-    <Header />
-    <Footer />
-    <Route
-              path="/signup"
-              element={
-                <Register
-                  handleUpdateRegistration={handleUpdateRegistration}
-                  message={message}
-                />
-              }
-            />
+      <div className="app__container">
+        <Routes>
+          <Route exact path="/" element={<Main loggedIn={loggedIn} />} />
+          <Route
+            path="/signup"
+            element={
+              <Register
+                handleUpdateRegistration={handleUpdateRegistration}
+                message={message}
+              />
+            }
+          />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+function Main(props) {
+  return (
+    <main>
+      <Header />
+      <Footer />
+    </main>
+  );
+}
+
+export default Main;
